57efb2365310871a0de6557910a281872c62761f
HarryXu497 <harryxu497@gmail.com>
2022-06-21 12:33:32 -0400

    added the GainAir entity type documentation

--- src/lib/actions/entity/GainAir.ts
+++ src/lib/actions/entity/GainAir.ts
@@ -11,6 +11,13 @@ interface GainAirObject {
     value: number;
 }
 
+/**
+ * 
+ * @description Restores breath (bubbles!) to a living entity.
+ * @param {number} value - 	The amount of breath to restore. 
+ * @returns The formatted object representing the entity action.
+ * @link {@link https://origins.readthedocs.io/en/latest/types/entity_action_types/gain_air/ External Documentation}
+ */
 export default function gainAir({ value }: GainAirObject) {
     return new GainAirAction(value);
 }
